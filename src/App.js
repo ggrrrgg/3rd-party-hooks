b34366a16197e1a278a75d4c55c0030533b18ce4
ggrrrgg <126563391+ggrrrgg@users.noreply.github.com>
2023-09-28 20:00:03 +1000

    sync local storage to state

---
+++ src/App.js
@@ -0,0 +1,43 @@
+
+import { useEffect, useState } from 'react';
+import './App.css';
+import { useLocalStorage } from 'react-use';
+
+function App() {
+
+
+  const [storeMessage, setStoreMessage] = useLocalStorage('message', '');
+  const [message, setMessage] = useState('');
+
+  // Component is loaded
+  // gap of time
+  // Component is rendered
+  // gap of time
+  // componentDidMount a.k.a useEffect(() => {}, [])
+  // gap of time 
+  // componentWillUpdate a.k.a useEffect(() => {}, [message])
+
+
+  useEffect(() => {
+      setMessage(storeMessage);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  useEffect(() => {
+    // /do something whenever message updates
+
+    setStoreMessage(message);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [message]);
+
+  return (
+    <div className="App">
+
+      {message && <h1>{message}</h1>}
+
+      
+    </div>
+  );
+}
+
+export default App;
